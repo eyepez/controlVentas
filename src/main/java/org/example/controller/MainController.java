f9498c61c473c0f891bb9b2d94090e2ae0651de3
eyepez <eyepez@canvia.com>
2023-07-27 23:22:52 -0500

    - depuracion de metodos en desuso - implementacion del metodo para guardar cliente

--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -1,13 +1,20 @@
 package org.example.controller;
 
+import org.example.model.Cliente;
+import org.example.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private ClienteService clienteService;
 
     @GetMapping("/")
     public String index(){
@@ -16,22 +23,22 @@ public class MainController {
 
     @GetMapping("/clientes")
     public String mostrarFormularioClientes(Model model) {
-        // Aquí puedes agregar lógica si es necesario
-        return "clientes"; // Devuelve el nombre de la vista (clientes.html)
+        model.addAttribute("cliente", new Cliente());
+        return "clientes";
     }
 
-//    @GetMapping("/formulario")
-//    public String mostrarFormulario() {
-//        return "ventaForm"; // Nombre de la plantilla Thymeleaf para mostrar el formulario
-//    }
-//
-//    @PostMapping("/calcularPrecioTotal")
-//    public String calcularPrecioTotal(@RequestParam int cantidad,
-//                                      @RequestParam String descripcion,
-//                                      @RequestParam double precioUnitario,
-//                                      Model model) {
-//        double precioTotal = cantidad * precioUnitario;
-//        model.addAttribute("precioTotal", precioTotal);
-//        return "ventaForm";
-//    }
+    @PostMapping("/clientes")
+    public String guardarCliente(@ModelAttribute Cliente cliente, Model model) {
+        System.out.println(cliente.toString());
+        try {
+            clienteService.guardarCliente(cliente);
+        } catch (Exception e) {
+            // Captura cualquier excepción relacionada con la base de datos
+            String errorMessage = "Ha ocurrido un error al guardar el cliente. Por favor, inténtalo nuevamente.";
+            model.addAttribute("errorMessage", errorMessage);
+            return "clientes"; // Regresa a la página de clientes con el mensaje de error
+        }
+        return "redirect:/clientes";
+    }
+
 }
